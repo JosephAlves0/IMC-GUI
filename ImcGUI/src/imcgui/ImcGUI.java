61afadbcb7d2eb7bfc48113117a2b8ccd864fdf7
José Eduardo <j.eduardo.alves@hotmail.com>
2021-10-25 22:48:36 -0300

    IMC com GUI

---
+++ ImcGUI/src/imcgui/ImcGUI.java
@@ -0,0 +1,10 @@
+package imcgui;
+
+public class ImcGUI {
+
+    public static void main(String[] args) {
+        ImcFramePrincipal frm = new ImcFramePrincipal();
+        frm.setVisible(true);
+    }
+    
+}
